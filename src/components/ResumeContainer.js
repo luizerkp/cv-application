e28d4222682dedb48f23890063e33fe411cbaa87
Luis Tamarez <luistamarez@hotmail.com>
2023-05-03 00:18:47 -0700

    Update resume template summary

--- src/components/ResumeContainer.js
+++ src/components/ResumeContainer.js
@@ -25,7 +25,9 @@ class ResumeContainer extends Component {
                         </div>
                     </div>
                     <div className={styles['resume-right']}>
-                        <div className={styles['resume-intro']}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div className={styles['resume-intro']}>
+                        Experienced Product Manager with a proven track record of successfully delivering products from conception to launch. Skilled in market research, product strategy, user experience design, project management, and team leadership. Strong ability to identify customer needs and translate them into product features that drive business growth and customer satisfaction. Proactive problem-solver with excellent communication and collaboration skills.
+                        </div>
                         <div className={styles['resume-experience']}>
                           <h1 className={styles['resume-section-title-right']}>Experience</h1>
                         </div>
