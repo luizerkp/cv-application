ba94f617bb1d33a20ac4dddecac7fbdbfa31e161
Luis Tamarez <luistamarez@hotmail.com>
2023-06-13 20:04:28 -0700

    Rename ExportPDF.js to GeneratePDF.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,14 @@ import logo from '../imgs/cv-logo.png';
 import { mdiFileDownload, mdiFormatListBulleted } from '@mdi/js';
 import Icon from '@mdi/react';
 import TemplateSelectModal from './TemplateSelectModal';
-import { Document, Page, pdf } from '@react-pdf/renderer';
-import { saveAs } from 'file-saver';
+
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modalOpen: false,
+      resumeFile: null,
     };
   }
   handleModalOpen = () => {
@@ -20,53 +20,40 @@ class Header extends Component {
   handleModalClose = () => {
     this.setState({ modalOpen: false });
   };
-  handleGeneratePDF = () => {
-    const resumeElement = document.querySelector('[data-resume]');
-    // console.log(resumeElement);
-    if (resumeElement) {
-      const pdf = (
-        <Document>
-          <Page>
-            {resumeElement}
-          </Page>
-        </Document>
-      );
 
-      const asBlob = pdf(pdf);
-      console.log(asBlob);
-      // saveAs(asBlob, 'resume.pdf');
-    }
-  };
+  // handleGeneratePDF = () => {
+  //   const resumeElement = document.querySelector('[data-resume]');
+  // };
   
   render() {
     const { currentTemplate, updateCurrentTemplate } = this.props;
     // console.log(currentTemplate);
       return (
-          <header className= {styles.header}>
-            <div className={styles ['header-left']}>
-              <img className= {styles.logo} src={logo} alt="Logo that reads CV" />
-              <h1 className={styles['gradient-text']}>CV Creator</h1>
-            </div>
-            <div className={styles['header-middle']}>
-              <button className={styles['header-button']} id='templates' onClick={this.handleModalOpen}
-              >
-                <Icon path={mdiFormatListBulleted} size={1} />
-                Templates
-              </button>
-            </div>
-            <div className={styles['header-right']} id='downloadPDF'>
-              <button className={styles['header-button']} onClick={this.handleGeneratePDF}>
-                <Icon path={mdiFileDownload} size={1} />
-                Generate PDF
-              </button>               
-            </div>
-            <TemplateSelectModal
-              isOpen={this.state.modalOpen}
-              onClose={this.handleModalClose}
-              currentTemplate={currentTemplate}
-              updateCurrentTemplate={updateCurrentTemplate}
-            />
-          </header>
+        <header className= {styles.header}>
+          <div className={styles ['header-left']}>
+            <img className= {styles.logo} src={logo} alt="Logo that reads CV" />
+            <h1 className={styles['gradient-text']}>CV Creator</h1>
+          </div>
+          <div className={styles['header-middle']}>
+            <button className={styles['header-button']} id='templates' onClick={this.handleModalOpen}
+            >
+              <Icon path={mdiFormatListBulleted} size={1} />
+              Templates
+            </button>
+          </div>
+          <div className={styles['header-right']} id='downloadPDF'>
+            <button className={styles['header-button']} onClick={this.handleGeneratePDF}>
+              <Icon path={mdiFileDownload} size={1} />
+              Generate PDF
+            </button>               
+          </div>
+          <TemplateSelectModal
+            isOpen={this.state.modalOpen}
+            onClose={this.handleModalClose}
+            currentTemplate={currentTemplate}
+            updateCurrentTemplate={updateCurrentTemplate}
+          />
+        </header>
       );
   }
 }
---
+++ src/utils/GeneratePDF.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class GeneratePDF extends Component {
+  handlePDFDocument = () => {
+    // const { resumeElement } = this.props;
+
+  };
+
+  render() {
+    return null;
+  }
+}
+
+
+export default GeneratePDF;
+
